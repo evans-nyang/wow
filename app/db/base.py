9a5f548ef3d4a3a161dbba33ffdc9fabd19dfc87
evans-nyang <nyanggithub@gmail.com>
2023-08-09 19:33:11 +0300

    Comment out test session

--- app/db/base.py
+++ app/db/base.py
@@ -1,20 +1,31 @@
 import os
 
+import pytest
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
+
 # Database connection URL
 DB_URL = os.environ.get('DATABASE_URL')
 if DB_URL is None:
     raise ValueError('DATABASE_URL environment variable not set')
 
+# Test database connection URL
+# TEST_DB_URL = os.environ.get('TEST_DATABASE_URL')
+# if TEST_DB_URL is None:
+#     raise ValueError('TEST_DATABASE_URL environment variable not set')
+
 # Create the database engine
 engine = create_engine(DB_URL)
 
+# Create the test database engine
+# test_engine = create_engine(TEST_DB_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
 Base = declarative_base()
 
@@ -25,3 +36,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Dependency injection for test database
+# @pytest.fixture(scope="module")
+# def get_test_db():
+#     db = TestSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
